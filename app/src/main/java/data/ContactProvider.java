0704d35c17076da2b5ef124f7e7915be7b0b7ff4
sanjuGoudru <sanjugr7022@gmail.com>
2018-09-22 12:31:33 +0530

    Contact Provider completed.
    Done by Sanjay

--- app/src/main/java/data/ContactProvider.java
+++ app/src/main/java/data/ContactProvider.java
@@ -9,6 +9,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
 import data.ContactContract.ContactEntry;
 import com.example.sushil.safe.ContactDbHelper;
 
@@ -102,16 +104,85 @@ public class ContactProvider extends ContentProvider {
     }
 
     private Uri insertContact(Uri uri, ContentValues contentValues) {
-        
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        String name = contentValues.getAsString(ContactEntry.COLUMN_CONTACT_NAME);
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name shouldn't be empty");
+        }
+
+        Integer number = contentValues.getAsInteger(ContactEntry.COLUMN_CONTACT_NUMBER);
+        if(number == null) {
+            throw new IllegalArgumentException("Number cannot be Invalid");
+        }
+
+        long id = database.insert(ContactEntry.TABLE_NAME, null, contentValues);
+        if(id == -1){
+            Log.e(TAG, "TEST: Failed to insert row for "+uri);
+            return null;
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri);
+        int rowsEffected;
+        switch(match) {
+            case CONTACTS:
+                rowsEffected = database.delete(ContactEntry.TABLE_NAME, selection, selectionArgs);
+                getContext().getContentResolver().notifyChange(uri, null);
+                return rowsEffected;
+            case CONTACTS_ID:
+                selection = ContactEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                rowsEffected = database.delete(ContactEntry.TABLE_NAME, selection, selectionArgs);
+                getContext().getContentResolver().notifyChange(uri, null);
+                return rowsEffected;
+            default:
+                throw new IllegalArgumentException("Invalid delete Uri: "+uri);
+        }
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String selection, @Nullable String[] selectionArgs) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case CONTACTS:
+                return updateContact(uri, contentValues, selection, selectionArgs);
+            case CONTACTS_ID:
+                // For the PET_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = ContactEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return updateContact(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    private int updateContact(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+
+        if(values.containsKey(ContactEntry.COLUMN_CONTACT_NAME)) {
+            String name = values.getAsString(ContactEntry.COLUMN_CONTACT_NAME);
+            if(name == null || name.isEmpty()) {
+                throw new IllegalArgumentException("Name shouldn't be empty");
+            }
+        }
+        if(values.containsKey(ContactEntry.COLUMN_CONTACT_NUMBER)) {
+            Integer number = values.getAsInteger(ContactEntry.COLUMN_CONTACT_NUMBER);
+            if(number == null){
+                throw new IllegalArgumentException("Number is Invalid");
+            }
+        }
+        if(values.size() == 0){
+            return 0;
+        }
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+        getContext().getContentResolver().notifyChange(uri, null);
+        return database.update(ContactEntry.TABLE_NAME, values, selection, selectionArgs);
     }
 }
