82a545c3b17dd1c8e6eda31cebb0f94629748e7c
Vasista <vasistadarbhe@gmail.com>
2018-09-22 11:00:45 -0700

    Created and Comleted ContactDbHelper.java class
    DOne by Sanjay in Vasista computer

--- app/src/main/java/com/example/sushil/safe/ContactDbHelper.java
+++ app/src/main/java/com/example/sushil/safe/ContactDbHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import data.ContactContract.ContactEntry;
+
 public class ContactDbHelper extends SQLiteOpenHelper {
 
     public static final String TAG = ContactDbHelper.class.getSimpleName();
@@ -19,10 +21,16 @@ public class ContactDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String SQL_CREATE_CONTACTS;
+        SQL_CREATE_CONTACTS = "CREATE TABLE "+ ContactEntry.TABLE_NAME + " ("
+                + ContactEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ContactEntry.COLUMN_CONTACT_NAME + " TEXT NOT NULL, "
+                + ContactEntry.COLUMN_CONTACT_NUMBER + " INTEGER NOT NULL UNIQUE, "
+                + ContactEntry.COLUMN_CONTACT_PRIORITY + " INTEGER NOT NULL DEFAULT "+ContactEntry.CONTACT_PRIORITY_FRIENDS+");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_CONTACTS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
     }
 }
